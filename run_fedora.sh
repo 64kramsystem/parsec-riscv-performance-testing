32c6839170c9020807c09cdc56e7406f643c388f
Saverio Miroddi <saverio.pub2@gmail.com>
2021-02-06 14:31:17 +0100

    run_fedora: Refactoring: Split into functions

--- run_fedora.sh
+++ run_fedora.sh
@@ -18,27 +18,32 @@ c_guest_image_diff=${c_guest_image_source/.qcow2/.diff.qcow2}
 c_kernel_image=$c_components_dir/Image
 c_bios_image=$c_components_dir/fw_dynamic.bin
 
-# Original Fedora configuration; requires `-append` to be removed.
-#
-# c_kernel_image=$c_components_dir/Fedora-Minimal-Rawhide-20200108.n.0-fw_payload-uboot-qemu-virt-smode.elf
-# c_bios_image=none
+function create_temp_image {
+  qemu-img create -f qcow2 -b "$c_guest_image_source" "$c_guest_image_diff"
+}
 
-qemu-img create -f qcow2 -b "$c_guest_image_source" "$c_guest_image_diff"
+function run_qemu {
+  # Original Fedora configuration; requires `-append` to be removed.
+  #
+  # c_kernel_image=$c_components_dir/Fedora-Minimal-Rawhide-20200108.n.0-fw_payload-uboot-qemu-virt-smode.elf
+  # c_bios_image=none
 
-echo 'Credentials (user:pwd) = riscv:fedora_rocks!'
+  "$c_qemu_binary" \
+    -nographic \
+    -machine virt \
+    -smp "$c_vcpus",cores="$c_vcpus",sockets=1,threads=1 \
+    -accel tcg,thread=multi \
+    -m "$c_guest_memory" \
+    -kernel "$c_kernel_image" \
+    -bios "$c_bios_image" \
+    -append "root=/dev/vda4 ro console=ttyS0" \
+    -object rng-random,filename=/dev/urandom,id=rng0 \
+    -device virtio-rng-device,rng=rng0 \
+    -device virtio-blk-device,drive=hd0 \
+    -drive file="$c_guest_image_diff",format=qcow2,id=hd0 \
+    -device virtio-net-device,netdev=usernet \
+    -netdev user,id=usernet,hostfwd=tcp::"$c_local_ssh_port"-:22
+}
 
-"$c_qemu_binary" \
-   -nographic \
-   -machine virt \
-   -smp "$c_vcpus",cores="$c_vcpus",sockets=1,threads=1 \
-   -accel tcg,thread=multi \
-   -m "$c_guest_memory" \
-   -kernel "$c_kernel_image" \
-   -bios "$c_bios_image" \
-   -append "root=/dev/vda4 ro console=ttyS0" \
-   -object rng-random,filename=/dev/urandom,id=rng0 \
-   -device virtio-rng-device,rng=rng0 \
-   -device virtio-blk-device,drive=hd0 \
-   -drive file="$c_guest_image_diff",format=qcow2,id=hd0 \
-   -device virtio-net-device,netdev=usernet \
-   -netdev user,id=usernet,hostfwd=tcp::"$c_local_ssh_port"-:22
+create_temp_image
+run_qemu
