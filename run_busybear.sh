f85fcbcf5320fefa1fd2589e025e2b1d59ead53d
Saverio Miroddi <saverio.pub2@gmail.com>
2021-02-06 14:47:58 +0100

    run_busybear: Refactoring: Split into functions

--- run_busybear.sh
+++ run_busybear.sh
@@ -19,18 +19,25 @@ c_guest_image_run=$c_temp_dir/busybear.run.qcow2
 c_kernel_image=$c_components_dir/Image
 c_bios_image=$c_components_dir/fw_dynamic.bin
 
-qemu-img create -f qcow2 -b "$c_guest_image_source" "$c_guest_image_run"
+function create_temp_image {
+  qemu-img create -f qcow2 -b "$c_guest_image_source" "$c_guest_image_run"
+}
 
-"$c_qemu_binary" \
-  -nographic \
-  -machine virt \
-  -smp "$c_vcpus",cores="$c_vcpus",sockets=1,threads=1 \
-  -accel tcg,thread=multi \
-  -m "$c_guest_memory" \
-  -kernel "$c_kernel_image" \
-  -bios "$c_bios_image" \
-  -append "root=/dev/vda ro console=ttyS0" \
-  -drive file="$c_guest_image_run",format=raw,id=hd0 \
-  -device virtio-blk-device,drive=hd0 \
-  -device virtio-net-device,netdev=usernet \
-  -netdev user,id=usernet,hostfwd=tcp::"$c_local_ssh_port"-:22
+function run_qemu {
+  "$c_qemu_binary" \
+    -nographic \
+    -machine virt \
+    -smp "$c_vcpus",cores="$c_vcpus",sockets=1,threads=1 \
+    -accel tcg,thread=multi \
+    -m "$c_guest_memory" \
+    -kernel "$c_kernel_image" \
+    -bios "$c_bios_image" \
+    -append "root=/dev/vda ro console=ttyS0" \
+    -drive file="$c_guest_image_run",format=raw,id=hd0 \
+    -device virtio-blk-device,drive=hd0 \
+    -device virtio-net-device,netdev=usernet \
+    -netdev user,id=usernet,hostfwd=tcp::"$c_local_ssh_port"-:22
+}
+
+create_temp_image
+run_qemu
