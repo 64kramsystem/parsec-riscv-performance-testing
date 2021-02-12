59197e0ca3d91824883f38b1e2473f1dd649ee56
Saverio Miroddi <saverio.pub2@gmail.com>
2021-02-12 17:50:58 +0100

    Add remaining PARSEC benchmark scripts

---
+++ support_scripts/bench_parsec_radiosity.sh
@@ -0,0 +1,12 @@
+c_input_type=simlarge
+
+# Input: $1=Number of threads.
+#
+function compose_benchmark_command {
+  local threads=$1
+
+  echo "
+    cd parsec-benchmark &&
+    HOSTTYPE=riscv64 bin/parsecmgmt -a run -p splash2x.radiosity -i $c_input_type -n $threads
+  "
+}
