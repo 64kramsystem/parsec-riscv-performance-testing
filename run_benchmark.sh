9535f513a82e7005b08971fbb76b207980056737
Saverio Miroddi <saverio.pub2@gmail.com>
2021-04-05 16:28:30 +0200

    Benchmark: Remove warmup functionality
    Good in theory, but not really helpful in practice; it's best to store the runs, then discard them after - inspecting the first runs can give helpful insight on what to consider warmup run.

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -37,7 +37,7 @@ c_qemu_pidfile=$c_temp_dir/$(basename "$0").qemu.pid
 
 c_debug_log_file=$(basename "$0").log
 
-c_help='Usage: '"$(basename "$0")"' [-s|--smt] [-w|--warmup <runs>] <bench_name> <runs> <qemu_boot_script> <benchmark_script>
+c_help='Usage: '"$(basename "$0")"' [-s|--smt] <bench_name> <runs> <qemu_boot_script> <benchmark_script>
 
 Runs the specified benchmark with different vCPU/thread numbers, and stores the results.
 
@@ -48,7 +48,6 @@ Example usage:
 Options:
 
 - `--smt`: enables SMT (the benchmark disables it by default)
-- `--warmup <runs>`: run extra <runs> at the beginning, without storing them in the results
 
 WATCH OUT! It'\''s advisable to lock the CPU clock (typically, this is done in the BIOS), in order to avoid the clock decreasing when the number of threads increase.
 
@@ -69,7 +68,6 @@ v_count_runs=  # int
 v_qemu_script= # string
 v_bench_script= # string
 v_smt_on=      # boolean (false=blank, true=anything else)
-v_warmup_runs=0 # int
 
 # Computed internally
 #
@@ -83,7 +81,7 @@ v_thread_numbers_list=()      # array
 ####################################################################################################
 
 function decode_cmdline_args {
-  eval set -- "$(getopt --options hsw: --long help,smt,warmup: --name "$(basename "$0")" -- "$@")"
+  eval set -- "$(getopt --options hs --long help,smt --name "$(basename "$0")" -- "$@")"
 
   while true ; do
     case "$1" in
@@ -93,9 +91,6 @@ function decode_cmdline_args {
       -s|--smt)
         v_smt_on=1
         shift ;;
-      -w|--warmup)
-        v_warmup_runs=$2
-        shift 2 ;;
       --)
         shift
         break ;;
@@ -151,19 +146,15 @@ function run_benchmark {
   #
   # Originally, the strategy was to use the run number in the outer cycle, with the rationale that variations
   # between runs would not cluster across a number of threads (inner cycle).
-  # Later, the nesting has been reversed, and an option for a warmup run has been added; this has been
-  # made possible by giving the guideline of setting a fixed CPU clock (and by the warmup functionality).
+  # Later, the nesting has been reversed; this has been made possible by giving the guideline of setting
+  # a fixed CPU clock.
   #
   for threads in "${v_thread_numbers_list[@]}"; do
     boot_guest "$threads"
     wait_guest_online
 
-    for ((run = 0 - v_warmup_runs; run < v_count_runs; run++)); do
-      if (( run < 0 )); then
-        local run_description="warmup($((run + v_warmup_runs)))"
-      else
-        local run_description=$run
-      fi
+    for ((run = 0; run < v_count_runs; run++)); do
+      local run_description=$run
 
       echo "Run:$run_description Threads:$threads..."
 
@@ -183,11 +174,9 @@ function run_benchmark {
         echo "-> TIME=$run_walltime"
       fi
 
-      if (( run >= 0 )); then
-        # Replaces time comma with dot, it present.
-        #
-        echo "$threads,$run,${run_walltime/,/.}" >> "$v_output_file_name"
-      fi
+      # Replaces time comma with dot, it present.
+      #
+      echo "$threads,$run,${run_walltime/,/.}" >> "$v_output_file_name"
     done
 
     shutdown_guest
