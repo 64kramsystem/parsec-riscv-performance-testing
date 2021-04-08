5dfca12930bef3d200471965d2df1d50a0399884
Saverio Miroddi <saverio.pub2@gmail.com>
2021-04-08 16:19:04 +0200

    Add run_isolpin_benchmarks.sh

---
+++ runner_scripts/run_isolpin_benchmarks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# No errexit; continue on error!
+set -o nounset
+
+if [[ $# -ne 2 || $1 == "--help" || $1 == "-h" ]]; then
+  echo '$1: runs, $2:qemu script'
+  exit 1
+fi
+
+runs=$1
+qemu_script=$2
+test_prefix=$(perl -ne 'print /qemu_(.+).sh/' <<< "$qemu_script")
+
+cd "$(readlink -f "$(dirname "$0")")"/..
+
+./run_benchmark.sh  "${test_prefix}_blackscholes"   "$runs" "$qemu_script" support_scripts/bench_parsec_blackscholes.sh
+./run_benchmark.sh  "${test_prefix}_bodytrack"      "$runs" "$qemu_script" support_scripts/bench_parsec_bodytrack.sh
+./run_benchmark.sh  "${test_prefix}_cholesky"       "$runs" "$qemu_script" support_scripts/bench_parsec_cholesky.sh
+./run_benchmark.sh  "${test_prefix}_freqmine"       "$runs" "$qemu_script" support_scripts/bench_parsec_freqmine.sh
+./run_benchmark.sh  "${test_prefix}_lu_cb"          "$runs" "$qemu_script" support_scripts/bench_parsec_lu_cb.sh
+./run_benchmark.sh  "${test_prefix}_lu_ncb"         "$runs" "$qemu_script" support_scripts/bench_parsec_lu_ncb.sh
+./run_benchmark.sh  "${test_prefix}_water_nsquared" "$runs" "$qemu_script" support_scripts/bench_parsec_water_nsquared.sh
+./run_benchmark.sh  "${test_prefix}_water_spatial"  "$runs" "$qemu_script" support_scripts/bench_parsec_water_spatial.sh
